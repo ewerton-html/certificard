5936833d2ed984b2caf70bd9c7ecdf0aa4780ad9
Ewerton Ribeiro da Silva <ewerton.silvatv96@gmail.com>
2021-10-05 02:57:45 -0300

    mudando foto-perfil e designe de links

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function mudaModo() {
     document.getElementById("instagram").src =
       "https://cdn-icons-png.flaticon.com/512/1051/1051364.png";
     document.getElementById("linkedin").src =
-      "https://cdn-icons-png.flaticon.com/512/126/126738.png";
+    "https://img-premium.flaticon.com/png/512/3128/premium/3128219.png?token=exp=1633413608~hmac=5620df6c9fb628d7eb82fd4e5b35a6a8";
     mudarCor = false;
   } else {
     document.getElementById("github").src =
@@ -15,7 +15,7 @@ function mudaModo() {
     document.getElementById("instagram").src =
       "https://cdn-icons-png.flaticon.com/512/1051/1051313.png";
     document.getElementById("linkedin").src =
-      "https://cdn-icons-png.flaticon.com/512/38/38669.png";
+      "https://cdn-icons-png.flaticon.com/512/61/61109.png";
     mudarCor = true;
   }
 }
